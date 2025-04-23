f0489bb4fadb0e76d7b796b4917b184fe881216b
Maruf khan <mk7446683@gmail.com>
2025-04-23 22:34:15 +0530

    Performance of app_id

--- Facebook SQL Interview Question.sql
+++ Facebook SQL Interview Question.sql
@@ -34,9 +34,63 @@ WITH CTE AS(
 
 SELECT
    app_id,
-   ROUND(ctr, 0)
+   ROUND(ctr, 2)
 AS
   ctr
 FROM
   CTE;
+  
+
+   
+   
+-- 1. **How successful is the ad campaign?**
+-- Write an SQL query to calculate the
+--  **percentage of impressions that resulted in clicks** across all apps.
+
+SELECT 
+   ROUND(COUNT(CASE WHEN event_type = 'click' THEN 1 END) * 100.00 / NULLIF(
+   COUNT(CASE WHEN event_type = 'impression' THEN 1 END), 0), 2
+   )
+   AS ctr_percentage
+FROM
+	app_click;
+    
+
+-- 2. **Which app performed better?**
+-- Compute the **click-through rate (CTR) for each app-id**
+--  (percentage of clicks out of total impressions per app).
+
+SELECT
+	app_id,
+    ROUND(COUNT(CASE WHEN event_type = 'click' THEN 1 END) * 100.00 / NULLIF(
+    COUNT(CASE WHEN event_type = 'impression' THEN 1 END), 0), 2) AS 
+    CTR
+FROM
+	app_click
+GROUP BY
+	app_id
+ORDER BY 
+	CTR DESC;
+    
+    
+-- 3. **Was the first app more successful than the second?**
+-- Find out which app-id had a higher 
+-- **conversion percentage** (clicks/impressions * 100).
+
+SELECT
+	app_id,
+    ROUND(COUNT(CASE WHEN event_type = 'click' THEN 1 END) * 100.00 / NULLIF(
+    COUNT(CASE WHEN event_type = 'impression' THEN 1 END), 0), 2)
+    AS Higher_conversion_percentage
+FROM
+	app_click
+WHERE
+	app_id IN(1, 2)
+GROUP BY 
+	app_id
+
+     
+  
+
+  
    
